19b1c1c0932178a628c9f2a706fcb6179361c44f
George Mandis <george@snaptortoise.com>
2017-09-22 21:51:15 -0700

    Refactored to utilize ble_midi Espruino module

--- puck-js-triple-click.js
+++ puck-js-triple-click.js
@@ -4,6 +4,8 @@ const urls = ["https://midi.mand.is", "https://george.mand.is", "https://snaptor
 var clickcount = 0;
 var clickevent = null;
 
+const midi = require("ble_midi");
+
 setWatch((e) => {
   clickcount++;
   if (clickevent !== null) clearTimeout(clickevent);
@@ -17,42 +19,22 @@ setWatch((e) => {
   } else {
     setLEDS(true, true, true);
 
-    // Initiate MIDI BLE connection 
-    NRF.setServices({
-      "03B80E5A-EDE8-4B33-A751-6CE34EC4C700": { // MIDI
-        "7772E5DB-3868-4112-A1A9-F2669D106BF3": {
-          readable: true,
-          writable: true,
-          notify: true,
-          value: [0x80, 0x80, 0x00, 0x00, 0x00]
-        }
-      }
-    });
-
-    NRF.setAdvertising([
-      // Flags: LE Limited Discoverable Mode, BR/EDR Not Supported
-      0x02, 0x01, 0x05,
-      // Complete Local Name: PuckCC
-      0x07, 0x09, 0x50, 0x75, 0x63, 0x6B, 0x43, 0x43,
-      // MIDI
-      0x11, 0x06, 0x00, 0xC7, 0xC4, 0x4E, 0xE3, 0x6C, 0x51,
-      0xA7, 0x33, 0x4B, 0xE8, 0xED, 0x5A, 0x0E, 0xB8, 0x03
-    ]);
+    midi.init();
   }
 
   clickevent = setTimeout(() => {
     if (clickcount === 1) {
       console.log("Click 1");
       NRF.nfcURL(urls[0]);
-      midiCC(CHANNEL, CONTROLLER, 127);
+      midi.send(CHANNEL, CONTROLLER, 127);
     } else if (clickcount === 2) {
       console.log("Click 2");
       NRF.nfcURL(urls[1]);
-      midiCC(CHANNEL, CONTROLLER + 1, 127);
+      midi.send(CHANNEL, CONTROLLER + 1, 127);
     } else if (clickcount === 3) {
       console.log("Click 3");
       NRF.nfcURL(urls[2]);
-      midiCC(CHANNEL, CONTROLLER + 2, 127);
+      midi.send(CHANNEL, CONTROLLER + 2, 127);
     }
 
     clickcount = 0;
@@ -73,17 +55,6 @@ setWatch((e) => {
   debounce: 10,
   repeat: true
 });
-
-const midiCC = (channel, controller, value) => {
-  NRF.updateServices({
-    "03B80E5A-EDE8-4B33-A751-6CE34EC4C700": { // MIDI
-      "7772E5DB-3868-4112-A1A9-F2669D106BF3": {
-        value: [0x80, 0x80, 0xB0 + channel, controller, value],
-        notify: true
-      }
-    }
-  });
-}
 
 const setLEDS = (LED1on, LED2on, LED3on) => {
   LED1.reset();
