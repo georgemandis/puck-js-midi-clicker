772e569b5af27c758d9018260ebb47fcfc85f4bf
George Mandis <george@mand.is>
2018-11-16 00:47:33 +0100

    Fixed flash disable

--- puck-js-triple-click.js
+++ puck-js-triple-click.js
@@ -1,105 +1,98 @@
-const CHANNEL = 10
-const CONTROLLER = 80
-const urls = [
-  'https://george.mand.is',
-  'https://snaptortoise.com',
-  'https://midi.mand.is'
-]
+const CHANNEL = 10;
+const CONTROLLER = 80;
+const urls = ["https://midi.mand.is", "https://george.mand.is", "https://snaptortoise.com"];
+let clickcount = 0;
+let clickevent = null;
+let flashLEDState = false;
+let flashLEDs = false;
 
-let clickcount = 0
-let clickevent = null
-let flashLEDState = false
-let flashLEDs = false
+const midi = require("ble_midi");
 
-const midi = require('ble_midi')
+    midi.init();
 
-midi.init()
 
-setWatch(
-  e => {
-    clickcount++
-    if (clickevent !== null) clearTimeout(clickevent)
+setWatch((e) => {
+  
 
+  
+  clickcount++;
+  if (clickevent !== null) clearTimeout(clickevent);
+
+  if (clickcount === 1) {
+    setLEDS(false, true, false);
+  } else if (clickcount === 2) {
+    setLEDS(true, false, false);
+  } else if (clickcount === 3) {
+    setLEDS(false, false, true);
+  } else {
+    setLEDS(true, true, true);
+  }
+
+  
+
+  clickevent = setTimeout(() => {
     if (clickcount === 1) {
-      setLEDS(false, true, false)
+      console.log("Click 1");
+      NRF.nfcURL(urls[0]);
+      midi.send(CHANNEL, CONTROLLER, 127);
+      disableFlashingLEDs();
     } else if (clickcount === 2) {
-      setLEDS(true, false, false)
+      console.log("Click 2");
+      NRF.nfcURL(urls[1]);
+      midi.send(CHANNEL, CONTROLLER + 1, 127);
+      disableFlashingLEDs();
     } else if (clickcount === 3) {
-      setLEDS(false, false, true)
-    } else {
-      setLEDS(true, true, true)
-    }
-
-    clickevent = setTimeout(() => {
-      if (clickcount === 1) {
-        console.log('Click 1')
-        NRF.nfcURL(urls[0])
-        midi.send(CHANNEL, CONTROLLER, 127)
-        disableFlashingLEDs()
-      } else if (clickcount === 2) {
-        console.log('Click 2')
-        NRF.nfcURL(urls[1])
-        midi.send(CHANNEL, CONTROLLER + 1, 127)
-        if (flashLEDs) clearInterval(flashLEDs)
-      } else if (clickcount === 3) {
-        console.log('Click 3')
-        NRF.nfcURL(urls[2])
-        midi.send(CHANNEL, CONTROLLER + 2, 127)
-        if (flashLEDs) clearInterval(flashLEDs)
-      } else if (clickcount === 4) {
-        console.log('Click 4')
-        midi.send(CHANNEL, CONTROLLER + 3, 127)
-        if (!flashLEDs) {
-          let flashLEDState = false
+      console.log("Click 3");
+      NRF.nfcURL(urls[2]);
+      midi.send(CHANNEL, CONTROLLER + 2, 127);
+      disableFlashingLEDs();
+    } else if (clickcount === 4) {
+      console.log("Click 4");
+      midi.send(CHANNEL, CONTROLLER + 3, 127);
+      if (!flashLEDs) {
+          let flashLEDState = false;
           flashLEDs = setInterval(() => {
-            flashLEDState = !flashLEDState
-            setLEDS(flashLEDState, !flashLEDState, false)
-          }, 100)
-        } else {
-          disableFlashingLEDs()
-        }
-      }
-      clickcount = 0
-    }, 350)
-  },
-  BTN,
-  {
-    edge: 'rising',
-    debounce: 10,
-    repeat: true
-  }
-)
+          flashLEDState = !flashLEDState;
+          setLEDS(flashLEDState, !flashLEDState, false);
+        },100);
+      }else{
+        disableFlashingLEDs();
+      }      
+    }
+    clickcount = 0;
+  }, 350);
+}, BTN, {
+  edge: "rising",
+  debounce: 10,
+  repeat: true
+});
 
-setWatch(
-  e => {
-    setLEDS(false, false, false)
-    setTimeout(() => {
-      clickevent = null
-    }, 400)
-  },
-  BTN,
-  {
-    edge: 'falling',
-    debounce: 10,
-    repeat: true
-  }
-)
+setWatch((e) => {
+  setLEDS(false, false, false);
+  setTimeout(() => {
+    clickevent = null;
+  }, 400);
+}, BTN, {
+  edge: "falling",
+  debounce: 10,
+  repeat: true
+});
 
 const disableFlashingLEDs = () => {
-  if (flashLEDs) {
-    clearInterval(flashLEDs)
-    flashLEDs = false
-    setLEDS(false, false, false)
+  if (flashLEDs) { 
+    clearInterval(flashLEDs);
+    flashLEDs = false;
+    setLEDS(false,false,false);
   }
 }
 const setLEDS = (LED1on, LED2on, LED3on) => {
-  LED1.reset()
-  LED2.reset()
-  LED3.reset()
+  LED1.reset();
+  LED2.reset();
+  LED3.reset();
 
-  if (LED1on) LED1.set()
-  if (LED2on) LED2.set()
-  if (LED3on) LED3.set()
-}
+  if (LED1on) LED1.set();
+  if (LED2on) LED2.set();
+  if (LED3on) LED3.set();
+};
 
-NRF.nfcURL(urls[0])
+NRF.nfcURL(urls[0]);
